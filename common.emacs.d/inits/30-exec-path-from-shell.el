ac20b9d628c159d49f408febfcfebd1fd2d9e165
shtaxxx <shta.ky1018@gmail.com>
2016-02-07 21:19:53 +0900

    30-exec-path-from-shell is updated

--- common.emacs.d/inits/30-exec-path-from-shell.el
+++ common.emacs.d/inits/30-exec-path-from-shell.el
@@ -1,5 +1,5 @@
+(custom-set-variables
+ '(exec-path-from-shell-check-startup-files nil))
 (exec-path-from-shell-initialize)
-
-;; General PATH and PATH for golang
 (let ((envs '("PATH" "GOPATH")))
   (exec-path-from-shell-copy-envs envs))
