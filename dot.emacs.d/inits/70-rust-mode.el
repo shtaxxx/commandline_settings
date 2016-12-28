ed725046cf75782c75e6c7a3cc921a2be189a059
shtaxxx <shta.ky1018@gmail.com>
2016-12-29 01:59:01 +0900

    Command check of racer

--- dot.emacs.d/inits/70-rust-mode.el
+++ dot.emacs.d/inits/70-rust-mode.el
@@ -1,2 +1,3 @@
-(add-hook 'rust-mode-hook '(lambda () (ac-racer-setup)))
-(setq racer-rust-src-path "/usr/local/src/rustc-1.14.0/src")
+(when (executable-find "racer")
+  (add-hook 'rust-mode-hook '(lambda () (ac-racer-setup)))
+  (setq racer-rust-src-path "/usr/local/src/rustc-1.14.0/src"))
