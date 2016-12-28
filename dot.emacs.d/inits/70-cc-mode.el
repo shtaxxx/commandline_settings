0bda561e3eefaf88a6d33e4189d73070de83056b
shtaxxx <shta.ky1018@gmail.com>
2016-12-29 01:29:59 +0900

    Check code for clang-complete-less environments.

--- dot.emacs.d/inits/70-cc-mode.el
+++ dot.emacs.d/inits/70-cc-mode.el
@@ -5,10 +5,11 @@
 
 (add-hook 'c-mode-common-hook
           '(lambda()
-             (require 'auto-complete-clang-async)
              (setq ac-clang-complete-executable "clang-complete")
-             (setq ac-sources '(ac-source-clang-async))
-             (ac-clang-launch-completion-process)))
+             (when (executable-find ac-clang-complete-executable)
+               (require 'auto-complete-clang-async)
+               (setq ac-sources '(ac-source-clang-async))
+               (ac-clang-launch-completion-process))))
           
 ;(add-hook 'c-mode-common-hook
 ;          '(lambda()
