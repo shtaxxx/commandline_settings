7e8d323d653197ccab0c4327a1b7016156a0b645
shtaxxx <shta.ky1018@gmail.com>
2016-12-29 11:41:15 +0900

    updated bashrc

--- dot.bashrc.bash
+++ dot.bashrc.bash
@@ -1,3 +1,20 @@
+# if bash does not exist, return
+[ "$BASH" = "" ] &&  return
+
+if [ -z "$PS1" ]; then
+    return
+fi
+
+# read system global settings
+if [ -f /etc/bashrc ]; then
+    source /etc/bashrc
+fi
+
+# read common settings
+if [ -f ../bashrc ]; then
+    source ../bashrc
+fi
+
 function share_history {
     history -a
     history -c
