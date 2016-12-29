173e0141de9d7d178e447b3560df613ee81daed4
shtaxxx <shta.ky1018@gmail.com>
2016-12-29 12:02:25 +0900

    dot.bash_sessions_disable

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 ROOT=~/.settings
 ln -s ${ROOT}/dot.bash_profile.bash .bash_profile
 ln -s ${ROOT}/dot.bashrc.bash .bashrc
+ln -s ${ROOT}/dot.bash_sessions_disable .bash_sessions_disable
 ln -s ${ROOT}/dot.screenrc .screenrc
 ln -s ${ROOT}/dot.emacs.d .emacs.d
 ln -s ${ROOT}/dot.vimrc .vimrc
