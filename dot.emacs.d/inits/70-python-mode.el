ee87d96d0f4edd2a45f6137dabfc9a133678f246
shtaxxx <shta.ky1018@gmail.com>
2020-12-31 19:26:42 +0900

    update

--- dot.emacs.d/inits/70-python-mode.el
+++ dot.emacs.d/inits/70-python-mode.el
@@ -1,5 +1,5 @@
 (defun my-python-mode-hook ()
-  ;; python common 
+  ;; common
   (setq python-python-command "python3")
   (setq indent-tabs-mode nil)
   (setq indent-level 4)
@@ -9,11 +9,11 @@
   (jedi:setup)
   (setq jedi:setup-keys t)
   (setq jedi:complete-on-dot t)
-  (setq jedi:environment-root "jedi")
+  (setq jedi:environment-root ".emacs.d/jedi")
   (setq jedi:environment-virtualenv
         (append python-environment-virtualenv '("--python" "python3")))
   (define-key jedi-mode-map (kbd "M-TAB") 'jedi:complete)
-  ;; py-autopep8
+  ;; autopep8
   (setq py-autopep8-options '("--ignore=E402" "--max-line-length=100")))
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
