ede4df6721081028d6a6edc158e55d83a65096f0
shtaxxx <shta.ky1018@gmail.com>
2025-01-10 20:56:01 +0900

    removing GOPATH

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -61,7 +61,7 @@
   :init
   (custom-set-variables '(exec-path-from-shell-check-startup-files nil))
   (exec-path-from-shell-initialize)
-  (let ((envs '("PATH" "GOPATH"))) (exec-path-from-shell-copy-envs envs)))
+  (exec-path-from-shell-copy-envs '("PATH")))
 
 ;; whitespace
 (use-package whitespace
